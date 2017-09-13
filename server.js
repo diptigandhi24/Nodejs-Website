ecd343407ae88db42640664588a89d581e7b3180
Dipti Gandhi <gandhidipti.s@gmail.com>
2017-09-13 17:12:46 +0530

    creating and handling the server using express js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-// import the http module
-var http = require('http');
-//handle sending request and returning responses
-function handleRequests(req , res){
-//return string
-	res.end("hello World") ; 
-}
-// create of server
-var server = http.createServer(handleRequests);
-// start server and return on port x
-server.listen(8080, function(){
-	console.log("listening on port 8080")
-})
+// // import the http module
+// var http = require('http');
+// //handle sending request and returning responses
+// function handleRequests(req , res){
+// //return string
+// 	res.end("hello World") ; 
+// }
+// // create of server
+// var server = http.createServer(handleRequests);
+// // start server and return on port x
+// server.listen(8080, function(){
+// 	console.log("listening on port 8080");
+// });
+
+
+///////////////Express JS ///////////////
+
+var express = require('express');
+
+//create new instance of it 
+
+var app = express();
+var port = 8080;
+
+//start the server
+
+app.listen(port, function(){
+	console.log('app started');
+});
